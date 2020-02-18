212b772eed3577c5febf3c915756cd8bbff0d52f
Sheriar <you@example.com>
2020-02-18 11:18:19 +0000

    project commit

---
+++ application/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = str(os.getenv("SECRET_KEY"))
+app.config['SQLALCHEMY_DATABASE_URI']=str(os.getenv("DB_URI"))
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from application import routes
+
